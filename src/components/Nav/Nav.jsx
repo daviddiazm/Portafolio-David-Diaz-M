8eb626bf53db160bb7434a0003071f8ca3f01bf6
david <davidcol123dm@gmail.com>
2024-12-22 14:18:27 -0500

    responsive nav y header funcional

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Nav.css"
 
 const Nav = () => {
+
+  const [toggleClass, setToggleClass] = useState("")
+
+  const changeToggle = () => {
+    if(toggleClass) {
+      setToggleClass("")
+    } else {
+      setToggleClass("nav__slide")
+    }
+  }
+
   return (
     // <nav className="nav__slide">
     <nav className="nav">
-      <button className="nav__toggel">
+      <button className="nav__toggel" onClick={changeToggle} >
         <i className="fa-solid fa-bars"></i>
       </button>
-      <ul className="nav__menu">
+      <ul className={"nav__menu "+ toggleClass}>
         <li className="menu__item">
           <a href="#" className="menu__link">Home</a>
         </li>
